0fd867acffbc0fc932ef684afacad9ba110ec557
Ayax50000 <fjlsoto@gmail.com>
2013-12-26 17:53:23 -0600

    identation in palindromo.rb fixed

--- palindromo.rb
+++ palindromo.rb
@@ -1,7 +1,7 @@
 class Palindromo
     def self.es_palindromo?(string)
-        stringdos = string.downcase.gsub(" ","")
-        stringuno = string.downcase.gsub(" ","").reverse
-        puts (stringuno == stringdos)? " es un palindromo" : "no es un palindromo"
+      stringdos = string.downcase.gsub(" ","")
+      stringuno = string.downcase.gsub(" ","").reverse
+      puts (stringuno == stringdos)? " es un palindromo" : "no es un palindromo"
     end
 end
