6dcfc626d415776b350e14320f078726e4833090
Ayax50000 <fjlsoto@gmail.com>
2013-12-26 17:47:44 -0600

    refactorized palindromo.rb

--- palindromo.rb
+++ palindromo.rb
@@ -1,16 +1,9 @@
 class Palindromo
     def initialize
     end
-    def self.test!(string)
-        stringdos = ""
-        stringuno = string
-        # el siguiente metodo convierte en minusculas el string
-        stringuno = stringuno.downcase
-        # el siguiente metodo remplaza " " por "" en el string
-        stringuno = stringuno.gsub(" ","")
-        # el siguiente metodo voltea el string 
-        stringdos = stringuno.reverse
-        puts "#{string} es un palindromo" if stringuno == stringdos
-        puts "#{string} no es un palindromo" unless stringuno == stringdos
+    def self.es_palindromo?(string)
+        stringdos = string.downcase.gsub(" ","")
+        stringuno = string.downcase.gsub(" ","").reverse
+        puts (stringuno == stringdos)? " es un palindromo" : "no es un palindromo"
     end
 end
